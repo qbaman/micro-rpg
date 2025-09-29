8dc33d6c2d05512c235ab02a8eb5fa7053dbe934
qbaman <78668489+qbaman@users.noreply.github.com>
2025-09-29 18:10:36 +0100

    feat: add Ant class with fields and constructor
    + Added Ant.cs with Name, Health, TextArt, Color, and ChargeDistance fields
    + Implemented constructor to initialize all fields and assign ASCII art
    - None

--- Ant.cs
+++ Ant.cs
@@ -4,35 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace InheritanceGameDemo
 {
     class Ant
     {
-        public string Name { get; set; }
-        public int Health { get; set; }
-        public int AttackPower { get; set; }
+        private string Name;
+        private int Health;
+        private string TextArt;
+        private ConsoleColor Color;
+        private int ChargeDistance;
 
-        public Ant(string name)
+        public Ant(string name, int health, ConsoleColor color, int chargeDistance)
         {
             Name = name;
-            Health = 20;
-            AttackPower = 5;
-        }
-
-        public void Display()
-        {
-            Console.WriteLine(ArtAssets.Ant);
-            Console.WriteLine($"Name: {Name} | Health: {Health} | Attack: {AttackPower}\n");
-        }
-
-        public void Attack(Ant target)
-        {
-            Console.WriteLine($"{Name} attacks {target.Name} for {AttackPower} damage!");
-            target.Health -= AttackPower;
-            if (target.Health <= 0)
-            {
-                Console.WriteLine($"{target.Name} has been defeated!\n");
-            }
+            Health = health;
+            Color = color;
+            ChargeDistance = chargeDistance;
+            TextArt = ArtAssets.Ant;
         }
     }
 }
